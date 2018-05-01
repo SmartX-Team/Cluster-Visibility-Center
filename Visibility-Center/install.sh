a7a481b7c490a77dcffc9a0485520f9d315c4c5c
Jun-Sik Shin <jsshin@smartx.kr>
2018-05-01 20:43:52 +0900

    Update Center & Client Installation Scripts

--- Visibility-Center/install.sh
+++ Visibility-Center/install.sh
@@ -1,10 +1,45 @@
 #!/usr/bin/env bash
 
+install_python_packages(){
+    apt update
+    apt install -y python-pip python-influxdb
+    pip install -r ./requirements.txt
+}
+
+install_influxdb(){
+    curl -sL https://repos.influxdata.com/influxdb.key | apt-key add -
+    source /etc/lsb-release
+    echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | tee /etc/apt/sources.list.d/influxdb.list
+
+    apt-get update
+    apt-get -y install influxdb
+    service influxdb start
+}
+
+install_grafana(){
+    echo "deb https://packagecloud.io/grafana/stable/debian/ stretch main" >> /etc/apt/sources.list
+    curl https://packagecloud.io/gpg.key | apt-key add -
+
+    apt-get update
+    apt-get -y install grafana
+
+    systemctl daemon-reload
+    systemctl start grafana-server.service
+    systemctl status grafana-server.service
+
+    systemctl enable grafana-server.service
+}
+
+install_grafana_plugin(){
+    grafana-cli plugins install grafana-worldmap-panel
+    systemctl restart grafana-server.service
+}
 if [ id -u -ne 0 ]; then
     echo "This script should be executed with ROOT privilege"
     exit 1
 fi
 
-apt update
-apt install -y python-pip python-influxdb
-pip install -r ./requirements.txt
+install_python_packages
+install_influxdb
+install_grafana
+install_grafana_plugin
--- Visibility-Client/install.sh
+++ Visibility-Client/install.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
 
+upgrade_kernel(){
+    uname -r | grep "4.15"
+    if [ $? -eq 0 ]; then
+        echo "Kernel was already upgraded to version 4.15"
+        return 0
+    fi
+
+    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15.18/linux-headers-4.15.18-041518-generic_4.15.18-041518.201804190330_amd64.deb
+    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15.18/linux-headers-4.15.18-041518_4.15.18-041518.201804190330_all.deb
+    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15.18/linux-image-4.15.18-041518-generic_4.15.18-041518.201804190330_amd64.deb
+
+    dpkg -i *.deb
+    rm *.deb
+    reboot
+}
 install_default_packages(){
     apt update
-    apt install -y python
+    apt install -y python python-pip
 }
 
 install_python_library(){
+    add-apt-repository -y ppa:maxmind/ppa
+    apt update
+    apt install libgeoip1 libgeoip-dev geoip-bin
     apt install -y python-influxdb
     pip install -r ./requirements.txt
 }
@@ -19,11 +37,12 @@ install_iovisor(){
     sudo apt-get install -y bcc-tools libbcc-examples
 }
 
-if [ id -u -ne 0 ]; then
+if [ `id -u` -ne 0 ]; then
     echo "This script should be executed with ROOT privilege"
     exit 1;
 fi
 
+upgrade_kernel
 install_default_packages
 install_python_library
 install_iovisor
